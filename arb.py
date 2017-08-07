19d442e1e8d2d45e7b3e99b5b6661b33cceb6868
nblackwood5 <nbla@sas.upenn.edu>
2017-08-06 21:37:14 -0400

    Add files via upload

--- arb.py
+++ arb.py
@@ -1,3 +1,29 @@
 #!/bin/env python
 
-#Find odds and exchange rates
+#Find arbitrages:
+#betting odds
+#foreign exhange markets
+#cryptocurrrency markets
+
+#forex-
+from forex_python.converter import CurrencyRates
+c = CurrencyRates()
+c.get_rates('USD')
+
+#bitcoin
+from forex_python.bitcoin import BtcConverter
+b = BtcConverter()
+b.get_latest_price('USD')
+
+'''
+A
+Mcgreggor: +500
+Money: -20
+
+B
+McG: +400
+Money: -25
+
+12.5 McG A -> 62.5
+50 Money B -> 62.5
+'''
